8f35bcd4a7331a6b9c67838a9fa6d5dc891fdaa8
AutumnV3 <150169477+AutumnV3@users.noreply.github.com>
2025-09-26 08:57:24 -0400

    Update Installer.lua

--- Installer.lua
+++ Installer.lua
@@ -7,13 +7,15 @@ table.foreach({"AutumnV3", "AutumnV3/Games", "AutumnV3/Configs"}, function(i, v)
 end)
 
 table.foreach({"GuiLibrary.lua", "Main.lua"}, function(i, v)
-    if not isfile(v) then
+    if not isfile('AutumnV3/ '.. v) then
         writefile("AutumnV3/" .. v, game:HttpGet(base .. v))
     end
 end)
 
-table.foreach({"BedwarZ"}, function(i, v)
-    if not isfile(v) then
+table.foreach({"BedwarZ.lua"}, function(i, v)
+    if not isfile('AutumnV3/Games/' .. v) then
         writefile("AutumnV3/Games/" .. v, game:HttpGet(base .. "/Games" .. v))
     end
 end)
+
+task.wait(1)
