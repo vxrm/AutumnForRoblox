763b11f42c653bfdcb9d2b53f9992446cd728867
AutumnV3 <150169477+AutumnV3@users.noreply.github.com>
2025-09-25 21:41:58 -0400

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,43 @@
+local GuiLibrary = loadfile('AutumnV3/GuiLibrary.lua')()
+
+GuiLibrary.Initialize();
+
+local Combat = GuiLibrary.API.CreateWindow('Combat')
+local Player = GuiLibrary.API.CreateWindow('Player')
+local Movement = GuiLibrary.API.CreateWindow('Movement')
+local Visuals = GuiLibrary.API.CreateWindow('Visuals')
+local World = GuiLibrary.API.CreateWindow('World')
+local Exploit = GuiLibrary.API.CreateWindow('Exploit')
+
+Uninject = Exploit:CreateModule({
+	['Name'] = 'Uninject',
+	['Function'] = function(callback)
+		if callback then
+			GuiLibrary.API.Uninject();
+			
+			if Reinject.Enabled then
+				loadfile('AutumnV3/Main.lua')()
+			end
+		end
+	end,
+})
+Reinject = Uninject.CreateToggle({
+	['Name'] = 'Reinject'
+})
+
+local Games = {
+    ['Bedwars'] = {6872274481, 8444591321, 8560631822},
+	['BedwarZ'] = {71480482338212},
+}
+
+loadfile('AutumnV3/Games/Universal.lua')()
+for gName, v in Games do
+    for _, id in v do
+        if game.PlaceId == id then
+            if not isfile('AutumnV3/Games/'..gName..'.lua') then
+                continue end;
+
+            loadfile('AutumnV3/Games/'..gName..'.lua')()
+        end
+    end
+end
